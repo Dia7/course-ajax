e118552c1cbca06e9bb64049dbaa88d80ba1ac52
Diana Ivanova Ratcheva <diana.ratcheva@gmail.com>
2018-06-08 09:59:19 +0300

    Adding Acync Articles from New York Times

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -1,23 +1,31 @@
 (function () {
     const form = document.querySelector('#search-form');
     const searchField = document.querySelector('#search-keyword');
-    // let searchedForText;
+    let searchedForText;
     const responseContainer = document.querySelector('#response-container');
-    var searchedForText = 'hippos';
 
     form.addEventListener('submit', function (e) {
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+        const unsplashRequest = new XMLHttpRequest();
+        unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+        unsplashRequest.onload = addImage;
+        unsplashRequest.onerror = function (err) {
+            requestError(err, 'image');
+        };
+        unsplashRequest.setRequestHeader('Authorization', 'Client-ID 9ae8c99681d73d47019c617956e205c3e00d36562aea8358f747fcbcbfd145cc');
+        unsplashRequest.send();
+
+        const articleRequest = new XMLHttpRequest();
+        articleRequest.onload = addArticles;
+        articleRequest.onerror = function (err) {
+            requestError(err, 'articles');
+        };
+        articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=cc86ccf1bac24bab9deab2c120e11230`);
+        articleRequest.send();
     });
-
-
-    var unsplashRequest = new XMLHttpRequest();
-
-    unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
-    unsplashRequest.onload = addImage;
-    unsplashRequest.setRequestHeader('Authorization', 'Client-ID 9ae8c99681d73d47019c617956e205c3e00d36562aea8358f747fcbcbfd145cc');
-    unsplashRequest.send();
 
     function addImage() {
         let htmlContent = '';
@@ -27,10 +35,28 @@
             const firstImage = data.results[0];
             htmlContent = `<figure>
                 <img src="${firstImage.urls.regular}" alt="${searchedForText}">
-                <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>`;
+                <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+                </figure>`;
         } else {
             htmlContent = '<div class="error-no-image">No images available</div>';
         }
         responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+        // debugger;
+    }
+
+    function addArticles() {
+        let htmlContent = '';
+        const data = JSON.parse(this.responseText);
+
+        if (data.response && data.response.docs && data.response.docs.length > 1) {
+            htmlContent = '<ul>' + data.response.docs.map(article => `<li class="article">
+                <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+                <p>${article.snippet}</p>
+                </li>`
+            ).join('') + '</ul>';
+        } else {
+            htmlContent = '<div class="error-no-articles">No articles Available</div>';
+        }
+        responseContainer.insertAdjacentHTML('beforeend', htmlContent);
     }
 })();
